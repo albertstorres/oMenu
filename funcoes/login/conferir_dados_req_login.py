329d0ed7717732077960f980da8afc6ef2e23438
Albert Sevy <albertstorres@gmail.com>
2024-04-22 21:42:47 -0300

    Excluindo imports da função conferir_dados_req_login

--- funcoes/login/conferir_dados_req_login.py
+++ funcoes/login/conferir_dados_req_login.py
@@ -1,5 +1,3 @@
-from flask import make_response, jsonify
-
 def conferir_dados_req_login (username, senha) :
     if not username or username == "" or not senha or senha == "" :
         return False
